8440f878bba177c8e2c7642e3ef3f9a41dbb25a3
Fat_Tonny <rumyn.abdula@gmail.com>
2018-02-28 09:37:14 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+int chislo=10;
+int result=0;
+result=chislo+chislo;
+        System.out.println(result);
+        System.out.println("Пошел нахуй");
+    }
+}
